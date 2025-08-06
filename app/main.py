9ea81b8d6182d4a3c05f165366b93c7f8c691537
Godfrey <ogembogodfrey75@gmail.com>
2025-08-06 23:45:48 +0300

    fix: PONG response for all sockets

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-import socket  # noqa: F401
-
+import socket  # noqa: F40
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,7 +9,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
     connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+    connection.sendall(b" +PONG\r\n ")
 
 
 
